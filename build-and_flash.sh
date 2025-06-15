5256d17e5cacd227515be7849564177735aa4388
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-15 19:38:17 +0700

    Update

---
+++ build-and_flash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e  # Dừng script nếu có lỗi
+echo "---------------------------------------------"
+echo "Build and flash the Sender example to board:"
+echo "---------------------------------------------"
+cd Examples/Sender
+rm -rf build
+mkdir build
+cd build
+cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=../../arm-none-eabi-gcc.cmake ..
+ninja
+PROGRAMMER="/home/trieunguyen/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI"
+echo "[INFO] Flashing firmware..."
+# Flash Node 001
+sudo "$PROGRAMMER" -c port=SWD sn=52FF6D064849825053310667 -w Node_01.bin 0x08000000 -v -rst
