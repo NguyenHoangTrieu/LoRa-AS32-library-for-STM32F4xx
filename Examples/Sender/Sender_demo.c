e2f735369ed63dde4c14104e2521b52b4a3d403f
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-19 22:01:11 +0700

    Update Sender_demo.c

--- Examples/Sender/Sender_demo.c
+++ Examples/Sender/Sender_demo.c
@@ -119,7 +119,7 @@ int main(void)
     // uint8_t len = UART_Read(rxBuffer, RX_BUFFER_SIZE, huart1);
     // if (len > 0)
     // {
-      HAL_UART_Transmit(&huart6, (uint8_t*)rxBuffer, strlen((char*)rxBuffer), 100);
+      HAL_UART_Transmit(&huart6, (uint8_t*)rxBuffer, 30, 100);
       // ClearString(rxBuffer, RX_BUFFER_SIZE);
       // len = 0;
       HAL_Delay(1000);
