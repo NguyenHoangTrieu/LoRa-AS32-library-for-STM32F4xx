a0ba7300d1c9895588ba1970100d10945ed0b32f
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-28 15:24:14 +0700

    Update build_and_flash.sh

--- build_and_flash.sh
+++ build_and_flash.sh
@@ -18,7 +18,10 @@ sudo "$PROGRAMMER" -c port=SWD sn=52FF71064849825056350467 -w Sender_demo.bin 0x
 sudo "$PROGRAMMER" -c port=SWD sn=52FF6D064849825053310667 -w Sender_demo.bin 0x08000000 -v -rst
 sudo "$PROGRAMMER" -c port=SWD sn=52FF6E064849825043360667 -w Sender_demo.bin 0x08000000 -v -rst
 sudo "$PROGRAMMER" -c port=SWD sn=52FF6C064849825033360667 -w Sender_demo.bin 0x08000000 -v -rst
-
+# List available serial ports
+echo "---------------------------------------------"
+echo "Available serial ports:"
+echo "---------------------------------------------"
 echo "Available serial ports:"
 ls /dev/tty*
 
