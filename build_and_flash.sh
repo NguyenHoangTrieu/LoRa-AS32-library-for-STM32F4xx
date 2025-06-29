8d3f76bad6a04adccb6967949610a8be31f2c410
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-29 23:53:13 +0700

    Update build_and_flash.sh

--- build_and_flash.sh
+++ build_and_flash.sh
@@ -15,7 +15,7 @@ echo "[INFO] Flashing firmware..."
 # Flash Node 001
 sudo "$PROGRAMMER" -c port=SWD sn=52FF6F064849825057460467 -w Sender_demo.bin 0x08000000 -v -rst
 sudo "$PROGRAMMER" -c port=SWD sn=52FF71064849825056350467 -w Sender_demo.bin 0x08000000 -v -rst
-#sudo "$PROGRAMMER" -c port=SWD sn=52FF6D064849825053310667 -w Sender_demo.bin 0x08000000 -v -rst 
+#sudo "$PROGRAMMER" -c port=SWD sn=52FF6D064849825053310667 -w Sender_demo.bin 0x08000000 -v -rst
 sudo "$PROGRAMMER" -c port=SWD sn=52FF6E064849825043360667 -w Sender_demo.bin 0x08000000 -v -rst
 sudo "$PROGRAMMER" -c port=SWD sn=52FF6C064849825033360667 -w Sender_demo.bin 0x08000000 -v -rst
 # List available serial ports
